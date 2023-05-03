acc67754cedffa008d62ff13c26894fec0b8e6d5
longstupay <761798834@qq.com>
2023-05-02 23:25:27 -1000

    chore: config tailwind and use appDir

---
+++ apps/dingtalk/app/layout.tsx
@@ -0,0 +1,16 @@
+import '../pages/globals.css';
+
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ apps/dingtalk/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      2333
+      <div className="text-red-500">7777777</div>
+    </div>
+  );
+}
