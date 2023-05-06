1e6a29fed6d392075d8a5f741e3779cb34767683
longstupay <761798834@qq.com>
2023-05-06 03:50:45 -1000

    chore: tw-prittie and base rectangle

--- apps/dingtalk/app/page.tsx
+++ apps/dingtalk/app/page.tsx
@@ -1,11 +1,15 @@
 import Carousels from '../components/Carousel';
 import NavBar from '../components/NavBar';
+import Rectangular from '../sections/Rectan-fede';
 
 export default function Page() {
   return (
     <div className="bg-slate-200 w-full min-h-[800px] overflow-hidden overflow-x-clip relative">
       <NavBar />
       <Carousels />
+      <div className="relative z-10 w-full overflow-x-clip">
+        <Rectangular />
+      </div>
       <div className="text-red-500">7777777</div>
     </div>
   );
---
+++ apps/dingtalk/sections/Rectan-fede.tsx
@@ -0,0 +1,92 @@
+function Rectangular() {
+  return (
+    <div className="overflow-hidden">
+      <div className="relative h-[200vh] w-full bg-black">
+        <div
+          id="colorBox-1-animation"
+          className="rect-xl left-[10%] top-[5%] bg-gradient-to-r from-cyan-500 to-blue-500 "
+        />
+        <div
+          id="colorBox-2-animation"
+          className="rect left-[45%] top-[1%] bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500"
+        />
+        <div
+          id="colorBox-3-animation"
+          className="rect left-[56%] top-[7%] bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-purple-700 via-blue-900
+       to-purple-800"
+        />
+        <div
+          id="colorBox-4-animation"
+          className="rect left-[26%] top-[15%] bg-[conic-gradient(at_top_right,_var(--tw-gradient-stops))] from-gray-200 via-blue-600 to-zinc-400 "
+        />
+        <div
+          id="colorBox5-animation"
+          className="rect left-[19%] top-[20%] bg-gradient-to-tl from-violet-400 via-purple-600 to-violet-900"
+        ></div>
+        <div
+          id="colorBox6-animation"
+          className="rect left-[22%] top-[30%] bg-gradient-to-tl from-yellow-300 via-amber-200 to-yellow-200"
+        ></div>
+        <div
+          id="colorBox7-animation"
+          className="rect left-[40%] top-[23%] bg-gradient-to-tl from-yellow-500 via-amber-500 to-yellow-500"
+        ></div>
+        <div
+          id="colorBox8-animation"
+          className="rect left-[36%] top-[30%] bg-gradient-to-tl from-yellow-700 via-amber-700 to-yellow-700"
+        ></div>
+        <div
+          id="colorBox9-animation"
+          className="rect left-[43%] top-[29%] bg-gradient-to-tl from-purple-300 via-violet-300 to-purple-600"
+        ></div>
+        <div
+          id="colorBox10-animation"
+          className="rect-xl left-[16%] top-[45%] bg-gradient-to-tl from-purple-300 via-violet-700 to-purple-800"
+        ></div>
+
+        <div
+          id="colorBox11-animation"
+          className="rect-xl right-[8%] top-[7%] bg-gradient-to-tl from-green-200 via-green-400 to-teal-400"
+        ></div>
+        <div
+          id="colorBox12-animation"
+          className="rect right-[8%] top-[10%] bg-gradient-to-tl from-sky-300 via-cyan-400 to-cyan-400"
+        ></div>
+        <div
+          id="colorBox13-animation"
+          className="rect right-[8%] top-[15%] bg-gradient-to-tl from-sky-300 via-cyan-600 to-cyan-700"
+        ></div>
+        <div
+          id="colorBox14-animation"
+          className="rect right-[8%] top-[20%] bg-gradient-to-bl from-sky-600 via-blue-400 to-cyan-400"
+        ></div>
+        <div
+          id="colorBox15-animation"
+          className="rect right-[8%] top-[25%] bg-gradient-to-bl from-indigo-600 via-blue-400 to-sky-600"
+        ></div>
+        <div
+          id="colorBox16-animation"
+          className="rect right-[8%] top-[30%] bg-gradient-to-bl from-indigo-800 via-blue-500 to-sky-600"
+        ></div>
+        <div
+          id="colorBox17-animation"
+          className="rect right-[8%] top-[35%] bg-gradient-to-bl from-purple-800 via-indigo-600 to-fuchsia-400"
+        ></div>
+        <div
+          id="colorBox18-animation"
+          className="rect right-[8%] top-[40%] bg-gradient-to-bl from-fuchsia-900 via-pink-700 to-pink-500"
+        ></div>
+        <div
+          id="colorBox19-animation"
+          className="rect right-[8%] top-[45%] bg-gradient-to-br from-rose-700 via-rose-500 to-rose-800"
+        ></div>
+        <div
+          id="colorBox20-animation"
+          className="rect right-[8%] top-[50%] bg-gradient-to-b from-lime-500 via-lime-700 to-lime-800 "
+        ></div>
+      </div>
+    </div>
+  );
+}
+
+export default Rectangular;
